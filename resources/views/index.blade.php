38e6e4fa2bda5da4f28fb7eacf1be3d17362f37e
Satyam Saw <satyamatibarts@gmail.com>
2023-11-24 01:59:32 +0530

    laravel basic route tutorial

---
+++ resources/views/index.blade.php
@@ -0,0 +1,5 @@
+<h1>Hello I'am a blade template.</h1>
+
+@isset($name)
+hello {{ $name }}
+@endisset
---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('index',[
+        // "name" => "stymsaw"
+    ]);
+});
+
+
+Route::get('/hello', function() {
+    return "hello";
+})->name('name');
+
+Route::get('/hello/{name}',function($name){
+    return "hello" . $name . "!";
+});
+
+Route::get('/hallo',function(){
+    return redirect('/hello')->route('hello');
+});
+
+Route::fallback(function() {
+    return "still got somewhere";
+});
