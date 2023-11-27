62f2ccb224f697245642e8ffca3c0b3fc5ebc93f
Satyam Saw <satyamatibarts@gmail.com>
2023-11-28 00:19:46 +0530

    saved the task with frontend side error handling and flash message

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,15 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @yield('styles')
     <title>Task List App</title>
 </head>
-<body>
 
+<body>
     <h1>@yield('title')</h1>
+    <div>
+        @if (session()->has('success'))
+            <div>{{ session('success') }}</div>
+        @endif
+    </div>
     <div>@yield('content')</div>
-    
+
 </body>
+
 </html>
