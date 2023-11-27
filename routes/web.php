0dfe2532365ed46b3e393adeb79d40664ae79aea
Satyam Saw <satyamatibarts@gmail.com>
2023-11-27 23:35:06 +0530

    created form to submit tasks

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@
 */
 
 use Illuminate\Http\Response;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -27,9 +28,22 @@ Route::get('/tasks', function () {
 })->name('tasks.index');
 
 
+Route::view('/tasks/create', 'create')
+->name('tasks.create');
+
 Route::get("/tasks/{id}", function ($id) {
     return view('show', ['task' => \App\Models\Task::findOrFail($id)]);
 })->name('tasks.show');
+
+
+Route::post('/tasks', function (Request $request) {
+
+    dd($request->all());
+
+})->name('tasks.store');
+
+
+
 
 Route::fallback(function () {
     return "still got somewhere";
