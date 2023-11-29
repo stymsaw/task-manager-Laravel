b0cce7aa3f07dceaf629c6a41e2d614218c37ac3
Satyam Saw <satyamatibarts@gmail.com>
2023-11-29 23:12:54 +0530

    added delete task feature

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -11,4 +11,15 @@
 
     <p>{{ $task->created_at }}</p>
     <p>{{ $task->updated_at }}</p>
+
+
+    <div>
+        <form method="POST" action="{{ route('tasks.destroy', ['task' => $task->id]) }}">
+            @csrf
+            @method('DELETE')
+            <button type="submit">Delete</button>
+
+        </form>
+    </div>
+
 @endsection
